17ba50faaea058595003a82661917f60d3aff821
github username <github email address>
2021-05-26 15:12:42 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import { addItem } from  './actions/items';
 class App extends Component {
 
   handleOnClick() {
-    this.props.store.dispatch(addItem());
+    this.props.addItem();
   }
 
   render() {
+    console.log('render')
     return (
       <div className="App">
         <button onClick={(event) => this.handleOnClick(event)}>
@@ -22,9 +23,17 @@ class App extends Component {
 };
 
 const mapStateToProps = (state) => {
+  console.log('mapState')
   return {
     items: state.items
   };
 };
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = (dispatch) => {
+  console.log('mapDispatch')
+  return {
+    addItem: () => dispatch(addItem())
+  } 
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
